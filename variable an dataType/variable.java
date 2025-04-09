c4a6111b7472ded5fb906a8ac88e71fb523deb64
Satyaprakash Sharma <sharmasatyaprakash169@gmai.com>
2025-04-10 01:06:59 +0530

    First java program

---
+++ variable an dataType/variable.java
@@ -0,0 +1,9 @@
+public class variable{
+    public static void main(String[] args) {
+        int a = 20;
+        String name = "Nick";
+        System.out.println(a);
+        System.out.println(name + a);
+
+    }
+}
