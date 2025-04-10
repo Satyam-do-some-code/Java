dec3baf780886c48fdc40fd8031067526db795b3
Satyaprakash Sharma <sharmasatyaprakash169@gmai.com>
2025-04-10 23:03:04 +0530

    learned about scanner class and made up some samll programs on scanner class.

---
+++ variable an dataType/readCharacter.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class readCharacter {
+    public static void main(String[] args) {
+     
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the Text :");
+        char charcter = sc.next().charAt(0);
+
+        System.out.println("Character is :" + charcter);
+
+        
+
+
+
+    }
+}
